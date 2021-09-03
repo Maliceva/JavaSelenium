69165062a90d540c1b3a4381fdc3062bec8e4925
Cassandra Reitz <39845336+Maliceva@users.noreply.github.com>
2021-09-02 19:06:54 -0500

    Updated dropdowns example

--- src/Dropdowns.java
+++ src/Dropdowns.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
+import java.util.List;
+
 public class Dropdowns {
 
     public static void main (String[] args) throws InterruptedException {
@@ -14,8 +16,9 @@ public class Dropdowns {
         driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
         driver.manage().window().maximize();
 
-        // ----------------------------------------------- //
+        // --------------------------------------------------------------- //
         // Static Dropdowns - consistent results regardless of user action //
+        // --------------------------------------------------------------- //
 
         //Handling static dropdown list with select tag
         WebElement staticDropdownCurrency = driver.findElement(By.xpath("//*[@id='ctl00_mainContent_DropDownListCurrency']"));
@@ -51,15 +54,34 @@ public class Dropdowns {
 
         // ----------------------------------------------- //
         // Dynamic Dropdowns - loaded based on user action //
+        // ----------------------------------------------- //
 
         //Open the "From" dynamic drop down
         driver.findElement(By.xpath("//*[@id='ctl00_mainContent_ddl_originStation1_CTXT']")).click();
         //Select Adampur (AIP) as the "From" location
         driver.findElement(By.xpath("(//a[@value='AIP'])[1]")).click();
         Thread.sleep(2000);
-        //Select Chennai (MAA) as the Destination (To)
-        driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+        //Select Chennai (MAA) as the Destination (To) using parent/child relationship
+        driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR'] //a[@value='MAA']")).click();
 
+        // -------------------------------------------------------------------- //
+        // AutoSuggestive Dropdowns - suggestions displayed based on user input //
+        // -------------------------------------------------------------------- //
+
+        //Start typing into the field
+        driver.findElement(By.id("autosuggest")).sendKeys("ind");
+        //After typing, wait to load suggestions
+        Thread.sleep(3000);
+        //Extract suggested options and save them in a list
+        List<WebElement> options = driver.findElements(By.xpath("//li[@class='ui-menu-item'] //a"));
+        //Find the option in the list that says "India" and click it, then end the loop
+        for(WebElement option : options){
+            if (option.getText().equalsIgnoreCase("India")){
+                option.click();
+                System.out.println("Clicked India option in the Country AutoSuggestive Dropdown");
+                break;
+            }
+        }
 
     }
 }
